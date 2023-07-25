e7add353fb23f6110266695495285f3167190cb0
Ronaldo Cerenza <rockronald@gmail.com>
2023-07-25 14:52:02 -0400

    montando a estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-export default function App() {
+import { useState } from 'react';
 
+export default function App() {
+const [todos, setTodos] = useState([
+  {
+    id: 1,
+    text: 'Fazer café',
+    category: 'Pessoal',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    text: 'Estudar React',
+    category: 'Estudos',
+    isCompleted: false
+  },
+  {
+    id: 3,
+    text: 'Criar um todo list',
+    category: 'Trabalho',
+    isCompleted: false
+  },
+])
   return (
-    <div className="bg-green-500 flex">
-      Olá Mundo!
+    <div className="bg-green-500">
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {todos.map(todo => (
+          <div className='todo' key={todo.id}>
+            <div className='content'>
+            <p>{todo.text}</p>
+            <p>({todo.category})</p>
+            </div>
+            <div>
+              <button>Concluir</button>
+              <button>Excluir</button>
+            </div>
+          </div>
+        ))}
+      </div>      
     </div>
   )
 }
