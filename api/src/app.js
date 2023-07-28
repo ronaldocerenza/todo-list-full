d4f7d66cb8927cd65c30a3af5ff700d92605fcb7
Ronaldo Cerenza <rockronald@gmail.com>
2023-07-28 17:00:21 -0400

    fs utils

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,36 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const { readTodo, writeTodo, updateTodo, deleteTodo } = require('./utils/fsUtils');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/todo', (_req, res) => {
-  res.status(200).json({ message: 'API Todolist OK' });
+app.get('/todo', async (_req, res) => {
+  const todo = await readTodo();
+  res.status(200).json(todo);
+});
+
+app.post('/todo', async (req, res) => {
+  const newTodo = req.body;
+
+  const newTodoId = await writeTodo(newTodo);
+  return res.status(201).json(newTodoId);
+});
+
+app.put('/todo/:id', async (req, res) => {
+  const { id } = req.params;
+  const upTodo = req.body;
+
+  const newTodo = await updateTodo(Number(id), upTodo);
+  return res.status(201).json(newTodo);
+});
+
+app.delete('/todo/:id', async (req, res) => {
+  const { id } = req.params;
+  await deleteTodo(Number(id));
+  
+  return res.status(204).end();
 });
 
 module.exports = app;
